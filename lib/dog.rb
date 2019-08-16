4a0c8ce8e40b83e08430a881e93137565637871e
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-08-16 01:59:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,25 @@
+class Dog
+
+ 	@@all = []
+	attr_accessor :name
+
+ 	def initialize(name)
+		@name = name
+		@@all << self
+	end
+
+ 	def self.clear_all
+		@@all.clear
+
+ 	end
+
+ 	def self.all
+		@@all.each do |dog|
+			puts dog.name
+		end
+	end
+end 
+
 
 # class Dog 
 
