fea34dc019a5718881025b82f73adfa0a23d3af2
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-08-16 01:59:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,77 @@
-# Add your code here
+
+class Dog 
+
+@@all = []
+
+attr_accessor :name
+
+def initialize(name)
+  @name = name 
+  @@all << self
+  save(@name)
+end
+
+def self.all
+  @@all.each do |d|
+    puts d.name
+  end 
+end
+
+def self.clear_all 
+  @@all.clear
+end 
+  
+def self.print_all
+   @@all.each do |dog|
+  puts dog.name 
+  end 
+end
+
+def save(name)
+  @@all << name
+end
+
+end
+  
+  
+
+  
+
+  
+#   def self.all 
+#     @@all.each do |song|
+#       puts song.name 
+#     end 
+#   end 
+# end 
+  
+#   ninety_nine_problems = Song.new("99 Problems")
+# Song.all
+
+
+
+# class Dog 
+# @@all = []
+# attr_accessor :name 
+
+# def initialize(name)
+# @name = name 
+# @@all << self 
+# end 
+
+# def self.all
+#   @@all.each do |dog|
+#     puts dog.name 
+#   end 
+# end  
+
+# def self.clear_all 
+# @@all.clear
+# end 
+# end 
+
+# Dog.new("Pluto")
+# Dog.new("Fido")
+# Dog.all
+# Dog.clear_all
+# Dog.all
